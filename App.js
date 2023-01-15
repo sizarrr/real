9e548b6d1c2cd0f4af8a540d1e099ff5bf480dd5
sizarrr <99047357+sizarrr@users.noreply.github.com>
2023-01-15 20:12:30 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Footer from './Footer'
+
+function App() {
+  return (
+   <section className=' mt-20 '>
+<Footer></Footer>
+
+</section>
+   
+  );
+}
+
+export default App;
